18c55d70617e431a5e16f2c3f18ac89e3f9a8997
Deepansh_5 <104337001+DeepanshK5@users.noreply.github.com>
2026-03-29 16:00:56 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
     private static List<String> quotes;
     
     public static void main(String[] args) throws IOException {
-        // Load quotes from an external file
+        // Load quotes from an external file -> doing changes here
         quotes = loadQuotesFromFile("quotes.txt");
         
         if (quotes.isEmpty()) {
